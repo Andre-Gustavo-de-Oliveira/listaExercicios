070664e6d60822cc6f209987450b1cc435cf5d29
André Oliveira <andre.oliveira.contato@outlook.com>
2026-04-09 20:51:53 -0300

    Exercicio Corrigido e Arrumado em Sua Branch

--- exercicio03.ConsoleApp/Program.cs
+++ exercicio03.ConsoleApp/Program.cs
@@ -4,6 +4,6 @@ Console.WriteLine("Digite a temperatura em graus Celsius: ");
 
 temperaturaCelsius = Convert.ToDecimal(Console.ReadLine());
 
-temperaturaFahrenheit = ((temperaturaCelsius - 9 / 5) + 32);
+temperaturaFahrenheit = ((temperaturaCelsius * 9 / 5) + 32);
 
 Console.WriteLine("A temperatura em graus Fahrenheit é: " + temperaturaFahrenheit);
