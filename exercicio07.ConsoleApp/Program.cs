18efe4f0d011031e1f37ddd38bd341ef301ad7d4
André Oliveira <andre.oliveira.contato@outlook.com>
2026-04-09 22:43:26 -0300

    Exercicio 07 Finalizado

--- exercicio07.ConsoleApp/Program.cs
+++ exercicio07.ConsoleApp/Program.cs
@@ -1,1 +1,76 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Digite seu peso em kg:");
+
+decimal peso = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Digite sua altura em metros:");
+
+decimal altura = Convert.ToDecimal(Console.ReadLine());
+
+decimal imc = peso / (altura * altura);
+
+Console.WriteLine("Seu IMC é: " + imc);
+
+
+if (imc < 18.5m)
+
+{
+
+    Console.WriteLine("Abaixo do peso");
+
+}
+
+if (imc < 18.5m)
+
+{
+
+    Console.WriteLine("Abaixo do peso");
+
+}
+
+else if (imc < 25)
+
+{
+
+    Console.WriteLine("Peso normal");
+
+}
+
+if (imc < 18.5m)
+
+{
+
+    Console.WriteLine("Abaixo do peso");
+
+}
+
+else if (imc < 25)
+
+{
+
+    Console.WriteLine("Peso normal");
+
+}
+
+else if (imc < 30)
+
+{
+
+    Console.WriteLine("Acima do peso");
+
+}
+
+else if (imc < 30)
+
+{
+
+    Console.WriteLine("Acima do peso");
+
+}
+
+else
+
+{
+
+    Console.WriteLine("Obeso");
+
+}
