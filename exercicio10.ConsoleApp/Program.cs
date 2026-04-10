bfafd6f008e834da543d3792ac109f47b0e3f713
André Oliveira <andre.oliveira.contato@outlook.com>
2026-04-09 23:11:57 -0300

    Exercicio 10 FInalizado

--- exercicio10.ConsoleApp/Program.cs
+++ exercicio10.ConsoleApp/Program.cs
@@ -1,1 +1,37 @@
-﻿Console.WriteLine("Hello, World!");
+﻿for (int i = 1; i <= 100; i++)
+
+{
+
+    if (i % 3 == 0 && i % 5 == 0)
+
+    {
+
+        Console.Write("FizzBuzz ");
+
+    }
+
+    else if (i % 3 == 0)
+
+    {
+
+        Console.Write("Fizz ");
+
+    }
+
+    else if (i % 5 == 0)
+
+    {
+
+        Console.Write("Buzz ");
+
+    }
+
+    else
+
+    {
+
+        Console.Write(Convert.ToString(i) + " ");
+
+    }
+
+}
